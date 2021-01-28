3d362574e9f6249002996346842303e71da888ad
xavierloos <xavier.loos04@gmail.com>
2021-01-28 14:13:49 +0000

    Post Home page styled

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,18 @@
 class PostsController < ApplicationController
-  def new
-    @post = Post.new
-  end
+  # def new
+  #   @post = Post.new
+  # end
 
   def create
     #change line below once user login working to User.find_by:
-    @user = User.create(username: "Natasha", email: "natasha@acebook.com", password: "bob")
+    @user = User.create(username: "Javi", email: "javi@acebook.com", password: "javi")
     @post = @user.posts.create(post_params)
     redirect_to posts_url
   end
 
   def index
-    @posts = Post.all.order('created_at DESC')
+    @post = Post.new
+    @posts = Post.all.order("created_at DESC")
   end
 
   private
