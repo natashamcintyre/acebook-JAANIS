f0ef58688bf86245a7b924a9377095d73be64658
Natasha McIntyre <natashamc.cs@gmail.com>
2021-01-28 00:14:05 +0000

    flash errors shown for incorrect sign up data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,17 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-    redirect_to posts_url, notice: "You have successfully signed up to Acebook!"
+
+    if @user.save
+      redirect_to posts_url, notice: "You have successfully signed up to Acebook!"
+    else
+      # This could be extracted into a helper method somewhere to keep controllers skinny
+      @user.errors.full_messages.each do |message|
+        flash.notice ||= []
+        flash.notice << message
+      end
+      redirect_to new_user_path
+    end
   end
 
   def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   # Ensures presence of username and email when user is saved into the database.
   # Use these in model to make application more secure.
   # Use Javascript in html to show to user if input is incorrect
-  validates :username, presence: true
-  validates :email, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
   validates :password, length: { in: 6..10 }
   # defines relationship between users and posts so that Active Record knows how
   # to interact with the database. (We are telling it a user's id could appear
