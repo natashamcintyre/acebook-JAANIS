769abdb6d774f9faaa75f90739f805a540dab9ee
Natasha McIntyre <natashamc.cs@gmail.com>
2021-01-28 11:11:22 +0000

    tests added

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.describe UsersController, type: :controller do
+  describe "GET /new" do
+    it "responds with 200 ok" do
+      get :new
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "POST /" do
+    it "responds with 200 and redirects to posts page" do
+      post :create, params: { user: { username: "Test User", email: "Test@example.com", password: "password" } }
+      expect(response).to redirect_to(posts_url)
+    end
+
+    it "creates a user" do
+      post :create, params: { user: { username: "Test User", email: "Test@example.com", password: "password" } }
+      expect(User.find_by(username: "Test User")).to be
+    end
+  end
+end
