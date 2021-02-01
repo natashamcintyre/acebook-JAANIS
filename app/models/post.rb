78247ce1f9c9488ca7fecd1ceebbb08850f9d11c
Natasha McIntyre <natashamc.cs@gmail.com>
2021-02-01 17:30:42 +0000

    remove unnecessary validation from post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,6 @@
 class Post < ApplicationRecord
   # assigns a user id to a creation of a post
   belongs_to :user
-  # ensures user is a valid user
-  validates_associated :user
   # ensure message can't be blank
   validates :message, presence: true
 end
