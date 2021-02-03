039f755897e12d43ca0854895334b93da81f005b
Ara_cho <47318984+Aracho1@users.noreply.github.com>
2021-02-04 00:19:12 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,20 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
+  def create
+    @user = User.create(user_params)
+    redirect_to login_url, notice: "You have successfully signed up to Acebook!"
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
 end
