316fcb8b51c42430b6b732e75a86cddf130c74e7
Sarah Power <sarahjonipower0@gmail.com>
2021-02-04 11:24:52 +0000

    like button on each post

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,7 +1,7 @@
     fetch('http://localhost:3000/api/v1/posts')
       .then(response => response.json())
       .then(data => renderPost(data));
-
+   
     function renderPost(data){
       var x  = document.getElementById('posts');
       data.forEach(function(post){
@@ -13,9 +13,35 @@
           <p class="post-text">${post.message}</p>
           <span class="post-date align-self-end">Posted by <span class="username"> ${post.user.username} </span> at
                ${post.created_at} </span>
-            
+            <div id="like-button"> </div>
         </div>
       </div>`
       x.insertAdjacentHTML('afterend', postHTML); 
+      renderLikeButton();
       });
   };
+
+  function renderLikeButton() {
+    const e = React.createElement;
+
+    class LikeButton extends React.Component {
+      constructor(props) {
+        super(props);
+        this.state = { liked: false };
+      }
+
+      render() {
+        if (this.state.liked) {
+          return 'You liked this.';
+        }
+
+        return e(
+          'button',
+          { onClick: () => this.setState({ liked: true }) },
+          'Like'
+        );
+      }
+    }
+    const domContainer = document.querySelector('#like-button');
+    ReactDOM.render(e(LikeButton), domContainer);
+  }
