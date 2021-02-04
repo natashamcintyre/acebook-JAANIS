1b77f368e90342617143159526d72c2982898ef1
xavierloos <xavier.loos04@gmail.com>
2021-02-04 22:48:03 +0000

    Styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,19 @@
 class UsersController < ApplicationController
-
   def new
     @user = User.new
   end
-  
-#   def create
-#     @user = User.create(user_params)
-#     redirect_to login_url, notice: "You have successfully signed up to Acebook!"
-#   end
+
+  #   def create
+  #     @user = User.create(user_params)
+  #     redirect_to login_url, notice: "You have successfully signed up to Acebook!"
+  #   end
 
   def create
     @user = User.create(user_params)
     p @user
     if @user.save
       session[:user_id] = @user.id
-      redirect_to posts_url, notice: "Logged in!"
+      redirect_to posts_url
     else
       render :new
     end
